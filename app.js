12145289cae3113c32d045f9ce54309b0c78414d
Jérémy Renaut <jeremy.renaut1@gmail.com>
2022-12-31 22:59:45 +0100

    Self pinging to keep the bot awake

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ import fetch from "node-fetch";
 const app = express();
 const port = 3000;
 const urlToPing = ["https://agrafes-server.onrender.com/api/products"];
+const self = "https://server-pinger-b.onrender.com";
 
 app.get("/", (req, res) => {
-  res.send("Hello World! :)");
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
   setInterval(() => {
     fetch(urlToPing).then(() => console.log(`Ping to ${urlToPing} done.`));
+    fetch(self).then(() => console.log(`Ping to ${self} done.`));
   }, 60000);
 });
