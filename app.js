77f4ace82e0b84592bd2222ebca28a4ce90391c4
Jérémy Renaut <jeremy.renaut1@gmail.com>
2023-02-12 17:03:06 +0100

    added the url to ping for my irclike project

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ import express from "express";
 import fetch from "node-fetch";
 const app = express();
 const port = 3000;
-const urlToPing = "https://agrafes-serv.onrender.com/api/products";
+const irclike = "https://irclike-server.onrender.com/api/messages";
+const agrafesServer = "https://agrafes-serv.onrender.com/api/products";
 const self = "https://server-pinger-projects.onrender.com";
 
 app.get("/", (req, res) => {
@@ -12,7 +13,10 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
   setInterval(() => {
-    fetch(urlToPing).then(() => console.log(`Ping to ${urlToPing} done.`));
+    fetch(agrafesServer).then(() =>
+      console.log(`Ping to ${agrafesServer} done.`)
+    );
+    fetch(irclike).then(() => console.log(`Ping to ${irclike} done.`));
     fetch(self).then(() => console.log(`Ping to ${self} done.`));
   }, 60000);
 });
